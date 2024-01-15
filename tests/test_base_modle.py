24bbeed040299b2f6332b4c3d4553073484a90e8
MohgaMA <mohga.m.ashour@gmail.com>
2024-01-15 14:26:26 -0800

    create the test base modle file

---
+++ tests/test_base_modle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" unittest for base model """
+
+import unittest
+import json
+from datetime import datetime
+
+class TestBaseModle(unittest.TestCase):
+     """ define unittests for base model """
+
+if __name__ == "__main__":
+    unittest.main()
